2bd1ce53a63bb08010b636c823161abc857e7207
Róbert Mesároš <mesarosrobo@gmail.com>
2016-12-31 00:49:40 +0100

    Version 1.0.4

--- Example_1/_js/sort_sk_cz.js
+++ Example_1/_js/sort_sk_cz.js
@@ -1,7 +1,7 @@
 /**
  * @package    Sort_SK_CZ JavaScript
- * @version    1.0.3
- * @date       Sun, 25 Dec 2016 20:44:07 +0100, Slovakia
+ * @version    1.0.4
+ * @date       Sun, 31 Dec 2016 00:46:12 +0100, Slovakia
  * @author     Robert Mesaros
  * @copyright  Copyright © 2016 Robert Mesaros, rmSOFT
  * @web        http://www.rmsoft.sk
